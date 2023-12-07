db5d1fc8e4a63ead2a378acc75733cfe9d8ae686
hiro1103 <96195951+hiro1103@users.noreply.github.com>
2023-12-07 10:42:00 +0900

    murakami!

--- src/index.js
+++ src/index.js
@@ -5,16 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 //import MyHello from "./chap03/MyHello";
 //import StateParent from "./chap03/StateParent";
-import EventMouse from "./chap03/EventMouse";
+//import EventMouse from "./chap03/EventMouse";
+import EventCompare from "./chap03/EventCompare";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <EventMouse
-    alt="ロゴ画像"
-    beforeSrc="https://www.web-deli.com/image/linkbanner_l.gif"
-    afterSrc="https://www.web-deli.com/image/home_chara.gif"
-  />
-);
+root.render(<EventCompare />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
