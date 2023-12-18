7400d411493d4653f1c3950e4269a57e6953fad6
hiro1103 <96195951+hiro1103@users.noreply.github.com>
2023-12-18 22:28:37 +0900

    20231218

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import reportWebVitals from "./reportWebVitals";
 //import MyHello from "./chap03/MyHello";
 //import StateParent from "./chap03/StateParent";
 //import EventMouse from "./chap03/EventMouse";
-import EventCompare from "./chap03/EventCompare";
+//import EventCompare from "./chap03/EventCompare";
+import EventPoint from "./chap03/EventPoint";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<EventCompare />);
+root.render(<EventPoint />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
